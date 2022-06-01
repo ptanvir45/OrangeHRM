a448b8c568a76877be90fd447112aca4a62b98b4
Tanvir Patwary <tanvirpatwary16@gmail.com>
2022-06-01 17:17:42 -0400

    second commit

--- src/test/java/com/batch24/steps/Home24Steps.java
+++ src/test/java/com/batch24/steps/Home24Steps.java
@@ -1,11 +1,7 @@
 package com.batch24.steps;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.batch24.pages.Home24Page;
@@ -13,39 +9,43 @@ import com.batch24.pages.Home24Page;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Home24Steps {
 	WebDriver driver;
-	Home24Page obj; 
+	Home24Page obj;
 
 	@Given("^user go to Orange HRM home page from batch (\\d+)$")
 	public void user_go_to_Orange_HRM_home_page_from_batch(int arg1) throws Throwable {
 
-		System.setProperty("webdriver.chrome.driver", "c:\\Driver\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+//		System.setProperty("webdriver.chrome.driver", "c:\\Driver\\chromedriver.exe");
 		driver = new ChromeDriver();
 
 		driver.get("https://opensource-demo.orangehrmlive.com/");
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);///----> Implicit wait 
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);///---->
+		// Implicit wait
 		driver.manage().window().maximize();
-		obj= new Home24Page(driver); 
+		obj = new Home24Page(driver);
+
 	}
 
 	@When("^user type the username$")
 	public void user_type_the_username() throws Throwable {
-		//WebElement Username = driver.findElement(By.xpath("//*[@id='txtUsername']"));
+		// WebElement Username = driver.findElement(By.xpath("//*[@id='txtUsername']"));
 		obj.getUsername().sendKeys("Admin");
 
 	}
 
 	@When("^user also type password$")
 	public void user_also_type_password() throws Throwable {
-		//WebElement password = driver.findElement(By.xpath("//*[@id='txtPassword']"));
+		// WebElement password = driver.findElement(By.xpath("//*[@id='txtPassword']"));
 		obj.getPassword().sendKeys("admin123");
 	}
 
 	@Then("^user click on the Login button$")
 	public void user_click_on_the_Login_button() throws Throwable {
-		//WebElement ClickLogin = driver.findElement(By.xpath("//*[@id='btnLogin']"));
+		// WebElement ClickLogin = driver.findElement(By.xpath("//*[@id='btnLogin']"));
 
 		obj.getLogin_button().click();
 
@@ -54,7 +54,7 @@ public class Home24Steps {
 	@Then("^user verify the welcome screen$")
 	public void user_verify_the_welcome_screen() throws Throwable {
 		String expected = "Welcome";
-		//String actual = driver.findElement(By.id("welcome")).getText();
+		// String actual = driver.findElement(By.id("welcome")).getText();
 		String actual = obj.getWelcomeTxt().getText();
 		Assert.assertTrue("Welcome message is not found", actual.contains(expected));
 	}
